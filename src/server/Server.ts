8c6077a87899611a36463a68b9bb3ec5cea38c58
Yuri <yurifurbino@gmail.com>
2023-09-20 14:18:50 -0300

    feat: added code style configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -3,9 +3,9 @@ import express from 'express';
 
 const server = express();
 
-server.get('/', (_, res) => {
 
-    return res.send('Olá, DEV!');
+server.get('/', (_, res) => {
+  return res.send('Olá, DEV!');
 });
 
 export { server };
