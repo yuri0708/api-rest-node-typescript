cdb11b73b1dd4ef4334f47f59bd4cb79df60909a
Yuri <yurifurbino@gmail.com>
2023-09-29 14:54:40 -0300

    fix: resolve production problem

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import { server } from './server/Server';
+import { Knex } from './server/database/knex';
+
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+  });
+};
 
 
-server.listen(process.env.PORT || 3333, () => {
-  console.log(`App rodando na porta ${process.env.PORT || 3333}`);
-});
+if (process.env.IS_LOCALHOST !== 'true') {
+  Knex.migrate.latest()
+    .then(() => {
+      startServer();
+    })
+    .catch(console.log);
+} else {
+  startServer();
+}
+
